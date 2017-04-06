a3379a9c673c742539b85db0f6ec20d546032010
Lisa Schoofs <lisaschoofs@gmail.com>
2017-04-06 15:01:49 -0500

    updats

--- client/scripts/client.js
+++ client/scripts/client.js
@@ -5,14 +5,33 @@ myApp.controller('OneController', ['$scope', 'MovieService', function($scope, Mo
 
 // The controller for the first view should have a button that 'grabs'
 // the information off of the form and stores it inside of a factory for the second view.
+// console.log(MovieService.movies);
+// console.log(MovieService.movie);
+    $scope.movie = {
+      title: '',
+      description: '',
+      director: '',
+      runTime: ''
+    };
+    //$scope.movies = MovieService.movies;
+    $scope.addMovie = MovieService.addMovie;
 
 
+    // $scope.addMovie = function() {
+    //   console.log(movie);
+    // };
 
 }]);
 
 
 myApp.controller('TwoController', ['$scope', 'MovieService', function($scope, MovieService){
 // grab movies from factory and make them available to view
+// console.log(MovieService.movies);
+// console.log(MovieService.movie);
+
+  // $scope.movie = MovieService.movie;
+  $scope.movies = MovieService.movies;
+  $scope.addMovie = MovieService.addMovie;
 
 
 }]);
@@ -22,8 +41,22 @@ myApp.factory('MovieService', [function(){
 
 
 
+var movieArray = [];
+
+var movies = {
+  movieArray: movieArray
+};
+
+
+function addMovie(newMovie) {
+  console.log("logging new movie: ", newMovie);
+  movieArray.push(newMovie);
+  console.log(movieArray);
+}
+
 //this makes it public
   return {
-
+    addMovie: addMovie,
+    movies: movies
   };
 }]);//ends factory

9e6f216147e5fd1402aa4ad0b1bbef3872c7f8b4
Lisa Schoofs <lisaschoofs@gmail.com>
2017-04-06 13:46:26 -0500

    initial setup

---
+++ client/scripts/client.js
@@ -0,0 +1,18 @@
+var myApp = angular.module('myApp', []);
+
+myApp.controller('OneController', ['$scope', function(){
+
+}]);
+
+
+myApp.controller('TwoController', ['$scope', function(){
+
+}]);
+
+
+myApp.factory('name', [function(){
+
+  return {
+
+  };
+}]);//ends factory

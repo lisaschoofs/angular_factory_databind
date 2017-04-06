9072983ce8024579b24087c115ea8a76dfa508e0
Lisa Schoofs <lisaschoofs@gmail.com>
2017-04-06 13:56:32 -0500

    controller updates

--- client/scripts/client.js
+++ client/scripts/client.js
@@ -1,17 +1,28 @@
 var myApp = angular.module('myApp', []);
 
-myApp.controller('OneController', ['$scope', function(){
+myApp.controller('OneController', ['$scope', 'MovieService', function($scope, MovieService){
+// inputs for movie name, description, director, length -->
+
+// The controller for the first view should have a button that 'grabs'
+// the information off of the form and stores it inside of a factory for the second view.
+
+
 
 }]);
 
 
-myApp.controller('TwoController', ['$scope', function(){
+myApp.controller('TwoController', ['$scope', 'MovieService', function($scope, MovieService){
+// grab movies from factory and make them available to view
+
 
 }]);
 
 
-myApp.factory('name', [function(){
+myApp.factory('MovieService', [function(){
 
+
+
+//this makes it public
   return {
 
   };

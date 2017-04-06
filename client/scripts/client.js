5ce58df1723bd3233d74c7eb1d838ca017749be1
Lisa Schoofs <lisaschoofs@gmail.com>
2017-04-06 17:24:36 -0500

    adds angular.copy

--- client/scripts/client.js
+++ client/scripts/client.js
@@ -2,33 +2,24 @@ var myApp = angular.module('myApp', []);
 
 myApp.controller('OneController', ['$scope', 'MovieService', function($scope, MovieService){
 // inputs for movie name, description, director, length -->
-
 // The controller for the first view should have a button that 'grabs'
 // the information off of the form and stores it inside of a factory for the second view.
 
+    // $scope.movie = MovieService.movie;
     $scope.movie = {
       title: '',
       description: '',
       director: '',
       runTime: ''
     };
-
     $scope.addMovie = MovieService.addMovie;
-
-
-    // $scope.addMovie = function() {
-    //   console.log(movie);
-    // };
 
 }]);
 
-
 myApp.controller('TwoController', ['$scope', 'MovieService', function($scope, MovieService){
 // grab movies from factory and make them available to view
-
   $scope.movies = MovieService.movies;
   $scope.addMovie = MovieService.addMovie;
-
 
 }]);
 
@@ -41,12 +32,14 @@ var movies = {
   movieArray: movieArray
 };
 
-
 function addMovie(newMovie) {
   console.log("logging new movie: ", newMovie);
-  movieArray.push(newMovie);
-  // console.log(movieArray);
-  // console.log(movies);
+  // angular.copy(movie, [destination]);
+  // angular.copy($scope.newMovie);
+  movieArray.push(angular.copy(newMovie));
+  //
+  console.log(movieArray);
+  console.log(movies);
 }
 
 //this makes it public
